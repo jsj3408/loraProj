3a5501d55d0b7f43356120a7d563556027b8370d
fireflies_at_night <js@interel.Com>
2023-05-19 21:55:29 +0400

    First commit

---
+++ source/app_config.h
@@ -0,0 +1,26 @@
+/*
+ * app_config.h
+ *
+ *  Created on: Feb 25, 2023
+ *      Author: johns
+ */
+
+#ifndef APP_CONFIG_H_
+#define APP_CONFIG_H_
+
+/**********************************************************************************************************************
+* Includes section
+*********************************************************************************************************************/
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "fsl_debug_console.h"
+#include "fsl_common.h"
+#include "fsl_clock.h"
+
+#include "fsl_port.h"
+#include "fsl_gpio.h"
+
+#endif /* APP_CONFIG_H_ */
